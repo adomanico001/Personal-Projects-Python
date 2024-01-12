29321e6f6b9515cb8a1ad36060f18bcc3f0ebeb7
Addie Domanico <adomanico001@gmail.com>
2024-01-12 16:09:18 -0500

    Add private instance variables

--- SeatReservation.java
+++ SeatReservation.java
@@ -7,5 +7,8 @@
  */
 
 public class SeatReservation {
-
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
 }
