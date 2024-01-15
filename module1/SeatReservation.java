20330b5ecef4e952ae2cde696f68e26e76b2f622
Addie Domanico <adomanico001@gmail.com>
2024-01-15 12:53:45 -0500

    Updated spelling errors

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,7 +1,7 @@
 
 /**
  * A program for an airline reservation management system.
- * Module 1 - Assignment 1, part 1
+ * Module 1 - Assignment 1
  * 
  * @author - Addie Domanico - CPSC 2710 - AO1
  * @version - 01/12/2024
