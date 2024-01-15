6407ba17332afa366907c0179cf423d4c53d4613
Addie Domanico <adomanico001@gmail.com>
2024-01-15 13:07:32 -0500

    Fixed spelling errors.

--- SeatReservation.java
+++ SeatReservation.java
@@ -6,9 +6,49 @@
  * @version - 01/12/2024
  */
 
+ import java.time.LocalDate;
+
 public class SeatReservation {
     private String flightDesignator;
-    private java.time.LocalDate flightDate;
+    private LocalDate flightDate;
     private String firstName;
     private String lastName;
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    public void setFlightDesignator(String fd) {
+        this.flightDesignator = fd;
+    }
+
+    public LocalDate getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(LocalDate date) {
+        this.flightDate = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String ln) {
+        this.lastName = ln;
+    }
+
+    public String toString() {
+        return "SeatReservation{" + "flightDesignator=" + flightDesignator + ", flightDate=" +
+            flightDate + ", firstName=" + (firstName != null ? "\"" + firstName + "\"" : "null") +
+            ", lastName=" + (lastName != null ? "\"" + lastName + "\"" : "null") + '}';
+    }
 }
