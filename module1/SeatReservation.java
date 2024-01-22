03e57147f642965c0e74606d100f7cd96e4bb51b
Addie Domanico <adomanico001@gmail.com>
2024-01-22 14:53:26 -0500

    Added new instance variables and updated toString method

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,54 +1,78 @@
+
 /**
  * A program for an airline reservation management system.
- * Module 1 - Assignment 1, part 1
+ * Module 1 - Assignment 1
  * 
  * @author - Addie Domanico - CPSC 2710 - AO1
  * @version - 01/12/2024
  */
 
- import java.time.LocalDate;
+import java.time.LocalDate;
 
 public class SeatReservation {
+    // Instance variables
     private String flightDesignator;
-    private LocalDate flightDate;
+    private java.time.LocalDate flightDate;
     private String firstName;
     private String lastName;
+    private int numberOfBags;
+    private boolean flyingWithInfant;
 
+    // Getters and setters
     public String getFlightDesignator() {
         return flightDesignator;
     }
 
     public void setFlightDesignator(String flightDesignator) {
+        if (flightDesignator == null) {
+            throw new IllegalArgumentException("flight designator cannot be null");
+        }
+
+        if (flightDesignator.length() < 4 || flightDesignator.length() > 6) {
+            throw new IllegalArgumentException("Flight designator must be 4-6 characters");
+        }
+
         this.flightDesignator = flightDesignator;
     }
-
     public LocalDate getFlightDate() {
         return flightDate;
     }
-
     public void setFlightDate(LocalDate date) {
         this.flightDate = date;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String fn) {
         this.firstName = fn;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String ln) {
         this.lastName = ln;
     }
-
-    public String toString() {
-        return "SeatReservation{" + "flightDesignator=" + flightDesignator + ", flightDate=" +
-            flightDate + ", firstName=" + (firstName != null ? "\"" + firstName + "\"" : "null") +
-            ", lastName=" + (lastName != null ? "\"" + lastName + "\"" : "null") + '}';
+    public int getNumberofBags() {
+        return numberOfBags;
     }
+    public void setNumberOfBags(int numberOfBags) {
+        this.numberOfBags = numberOfBags;
+    }
+    public boolean isFlyingWithInfant() {
+        return flyingWithInfant;
+    }
+    public void makeFlyingWithInfant() {
+        this.flyingWithInfant = true;
+    }
+    public void makeNotFlyingWithInfant() {
+        this.flyingWithInfant = false;
+    }
+    public String toString() {
+        return String.format("SeatReservation{flightDesignator=%s, flightDate=%s, firstName=%s, lastName=%s}",
+                flightDesignator, flightDate, firstName != null ? "\"" + firstName + "\"" : "null",
+                lastName != null ? "\"" + lastName + "\"" : "null", numberOfBags, flyingWithInfant);
+    }
+
 }
+
+    
