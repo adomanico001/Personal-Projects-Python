94935cab3351ed6d7267df1c996e4d60df9f5884
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-05-05 18:46:16 -0400

    Upload death_valley_highs_lows.py

---
+++ Data Visualization/Environmental Infographs/death_valley_highs_lows.py
@@ -0,0 +1,43 @@
+# Infograph for high/low temperatures - Death Valley, CA
+# @author Addie Domanico
+# @version 05/05/2024
+
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates and high/low temperatures from csv file
+    dates, highs, lows = [], [], []
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# Plot the high and low temperatures
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+
+# Format plot
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
